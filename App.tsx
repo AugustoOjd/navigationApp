898d9faa78b73039a9072a2deaf3b62ed38f89a7
Augusto Ojeda <okarinex@gmail.com>
2022-11-14 22:12:56 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>
+        App Screen
+      </Text>
+    </View>
+  )
+}
+
+export default App
